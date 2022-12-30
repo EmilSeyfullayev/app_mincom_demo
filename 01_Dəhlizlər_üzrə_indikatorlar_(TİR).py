1acbd37bf0ee9ba5822c72ae5dcf209cc9c0ec8b
Emil Seyfullayev <67646591+EmilSeyfullayev@users.noreply.github.com>
2022-12-30 15:11:12 +0400

    Add files via upload

--- 01_Dəhlizlər_üzrə_indikatorlar_(TİR).py
+++ 01_Dəhlizlər_üzrə_indikatorlar_(TİR).py
@@ -1,19 +1,21 @@
 import streamlit as st
 from a_read_db import data_for_metrics, max_year_month_pre_year
 
-
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+st.markdown("<h6 style='text-align: right; color: #075586;'>©Nəqliyyat siyasəti şöbəsi - Tranzit yükdaşımalar</h6>",
+        unsafe_allow_html=True)
+
 data_for_metrics = data_for_metrics()
 max_year = max_year_month_pre_year()[0]
 max_month = max_year_month_pre_year()[1]
 
-# Row A
-# st.markdown('## Dəhlizlər üzrə tranzit yüklərin həcmi, min ton')
-st.markdown(f'## Dəhlizlər üzrə cari ilin {max_month} ayı ilə ötən ilin analoji dövrü ilə müqayisəsi, ümumi həcm, min ton ilə')
+
+st.markdown(f'###### Dəhlizlər üzrə cari ilin {max_month} ayı ilə ötən ilin analoji dövrü ilə müqayisəsi, ümumi həcm, min ton ilə')
+
 col1, col2, col3 = st.columns(3)
 col1.metric(list(data_for_metrics.keys())[0], data_for_metrics[list(data_for_metrics.keys())[0]][0],
                                               data_for_metrics[list(data_for_metrics.keys())[0]][1])
