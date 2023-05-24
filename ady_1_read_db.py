1771df3e6de3380f543b2a52955532b91b93f2ba
Emil Seyfullayev <67646591+EmilSeyfullayev@users.noreply.github.com>
2023-05-24 09:34:59 +0400

    Update ady_1_read_db.py

--- ady_1_read_db.py
+++ ady_1_read_db.py
@@ -4,7 +4,7 @@ import sqlite3
 import datetime
 import streamlit as st
 
-path = 'ady_For_App_all_regimes_2017_2023_March.db'
+path = 'ady_For_App_all_regimes_2017_2023_April.db'
 
 
 @st.cache(allow_output_mutation=True)
